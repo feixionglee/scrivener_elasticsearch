a2c8f3316b69a2996f010f07d80531abd0511c61
lifeixiong <feixiongli@gmail.com>
2016-06-17 12:04:26 +0800

    struct for elasticsearch hits

--- lib/scrivener_elasticsearch.ex
+++ lib/scrivener_elasticsearch.ex
@@ -1,15 +1,18 @@
 defmodule ScrivenerElasticsearch do
+  use Scrivener, page_size: 10
+
+  defstruct _id: "", _index: "", _score: 0.0, _source: %{}, _type: ""
 end
 
 
-defimpl Scrivener.Paginater, for: ScrivenerElasticsearch do
+defimpl Scrivener.Paginater, for: Map do
 
   alias Scrivener.{Config, Page}
 
   @spec paginate(Map, Scrivener.Config.t) :: Scrivener.Page.t
   def paginate(es_result, %Config{page_size: page_size, page_number: page_number, module: ScrivenerElasticsearch}) do
 
-    entries = es_result.hits.hits
+    entries = Enum.map es_result.hits.hits, fn(v) -> struct(ScrivenerElasticsearch, v) end
     total_entries = es_result.hits.total
 
     %Page{
