1d8946ffc1729ee7f2d277b74c4adfe0d11c2281
lifeixiong <feixiongli@gmail.com>
2016-06-17 16:35:34 +0800

    fix Scrivener.Config args, implement __using__ macro

--- lib/scrivener_elasticsearch.ex
+++ lib/scrivener_elasticsearch.ex
@@ -1,16 +1,18 @@
 defmodule ScrivenerElasticsearch do
-  use Scrivener, page_size: 10
+  defstruct _id: "", _index: "", _score: 0.1, _source: %{}, _type: ""
 
-  defstruct _id: "", _index: "", _score: 0.0, _source: %{}, _type: ""
+  defmacro __using__(opts) do
+    quote do
+      use Scrivener, unquote(opts)
+    end
+  end
 end
 
-
 defimpl Scrivener.Paginater, for: Map do
-
   alias Scrivener.{Config, Page}
 
   @spec paginate(Map, Scrivener.Config.t) :: Scrivener.Page.t
-  def paginate(es_result, %Config{page_size: page_size, page_number: page_number, module: ScrivenerElasticsearch}) do
+  def paginate(es_result, %Config{page_size: page_size, page_number: page_number, module: _scrivener_elasticsearch}) do
 
     entries = Enum.map es_result.hits.hits, fn(v) -> struct(ScrivenerElasticsearch, v) end
     total_entries = es_result.hits.total
